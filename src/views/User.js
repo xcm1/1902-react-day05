bae362cd5e445024b6f0c6bc624f22982182b82e
xcm1 <121969209@qq.com>
2019-06-15 17:58:04 +0800

    页面基本布局

---
+++ src/views/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Com extends React.Component {
+  render () {
+    return (
+      <div className = "box">
+        <header className = "header">个人头部</header>
+        <div className = "content">个人内容</div>
+      </div>
+    )
+  }
+}
+
+export default Com;
