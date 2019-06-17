a16c406f4601cfb332342f2b2b197be04b8cd5fa
xcm1 <121969209@qq.com>
2019-06-17 10:33:32 +0800

    数据的请求渲染到home --声明式渲染

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import App from '@/layout/App';
+import DetailApp from '@/layout/DetailApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <Switch>
+      <Route path = '/detail' component = { DetailApp } />
+      <Route path = '/' component = { App } />
+    </Switch>
+  </HashRouter>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/layout/DetailApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function DetailApp () {
+  return (
+    <div className = "container">
+      <div className = "box">
+        <header className = "header">详情头部</header>
+        <div className = "content">详情内容</div>
+      </div>
+      <footer className = "footer">详情底部</footer>
+    </div>
+  )
+}
+
+export default DetailApp;
